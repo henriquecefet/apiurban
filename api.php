422d222a17d2e3725e27e0865e94bbad5058b795
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-12-23 16:29:54 -0300

    Add files via upload

--- api.php
+++ api.php
@@ -1,22 +1,25 @@
 <?php
 	$cidade = $_GET['cidade'];
-	lerCidade($cidade);
-	lerClima($cidade);
-
-	function lerCidade($cidade){
-		$url  = "https://urbanweb.herokuapp.com/apilerhotspot.php?nome=".urlencode($cidade);;
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_URL,$url);
-		$result=curl_exec($ch);
-		curl_close($ch);
-		$dados = json_decode($result, true);
-		echo '<pre>'; print_r($dados); echo '</pre>';
-		echo "<br>";
+	$hotspots = lerJSON("https://urbanweb.herokuapp.com/apilerhotspot.php?nome=", $cidade);
+	$tempo = lerJSON("https://api.hgbrasil.com/weather?key=da6e4d4b&city_name=", $cidade);
+	$recomendacaoHoje  = "";
+	for($i = 0; $i < count($hotspots["hotspot"]); $i++){
+		if($hotspots["hotspot"][$i]["ar-livre"] == t){
+			if($tempo["results"]["description"] == "Tempestestades" || $tempo["results"]["description"] == "Tempestestades Isoldas"){
+				$recomendacaoHoje = "Não recomendado";
+			}
+			elseif($tempo["results"]["description"] == "Tempo nublado" || $tempo["results"]["description"] == "Trovoadas Dispersas") {
+				$recomendacaoHoje  = "Pouco recomendado";
+			}
+		}
+		else{
+			$recomendacaoHoje  = "Recomendado";
+		}
+		$hotspots["hotspot"][$i]["recomedacao"] = $recomendacaoHoje;
 	}
-	function lerClima($cidade){
-		$url  = "https://api.hgbrasil.com/weather?key=da6e4d4b&city_name=".urlencode($cidade);;
+	echo json_encode($hotspots);
+	function lerJSON($link, $cidade){
+		$url  = $link."".urlencode($cidade);
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -24,7 +27,6 @@
 		$result=curl_exec($ch);
 		curl_close($ch);
 		$dados = json_decode($result, true);
-		echo '<pre>'; print_r($dados); echo '</pre>';
-		echo "<br>";
+		return $dados;
 	}
 ?>
