5bb7d9267915560148f752884f357f16fcf32ee8
henriquecefet <rodriguesoareshenrique@gmail.com>
2021-01-18 15:46:01 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
                 for(let i = 0; i<jx.cidades.length; i++){
                 	 let pai = adicionarDiv("row", "pai"+jx.cidades[i].nome, "card mb-4 shadow-sm")
                 	 let id = adicionarDiv(pai, jx.cidades[i].nome, "card-body");
-                	 adicionarImagem(id,jx.cidades[i].imagem, "200px", "400px")
+                	 adicionarImagem(id,jx.cidades[i].imagem, "200px", "350px")
         			 adicionarParagrafo(id, jx.cidades[i].nome);
                 }
             });
