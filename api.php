f2d65087ebcf2e197154fcab1d9534f5f2fa5e07
henriquecefet <rodriguesoareshenrique@gmail.com>
2021-01-21 12:26:54 -0300

    Add files via upload

--- api.php
+++ api.php
@@ -434,15 +434,20 @@ EOF;
                         }
                         if($recomendacaoData == "Nao recomendado" && $recomendacaoCovid == "Pouco recomendado") {
                             $recomendacaoGeral = "Nao recomendado";
+                             echo $recomendacaoGeral."<br>";
                         } elseif($recomendacaoData == "Nao recomendado" && $recomendacaoCovid == "Recomendado") {
                              $recomendacaoGeral = "Nao recomendado";
+                             echo $recomendacaoGeral."<br>";
                         } elseif($recomendacaoData = "Pouco recomendado" && $recomendacaoCovid == "Recomendado"){
                              $recomendacaoGeral = "Pouco recomendado";
+                             echo $recomendacaoGeral."<br>";
                         }elseif($recomendacaoCovid == "Nao recomendado"){
                             $recomendacaoGeral = "Nao recomendado";
+                            echo $recomendacaoGeral."<br>";
                         }
                         else{
                             $recomendacaoGeral = $recomendacaoData;
+                            echo $recomendacaoGeral."<br>";
                         }
                         array_push($previsaoData,  $recomendacaoGeral);
                     } elseif ($hotspots["hotspot"][$i]["ar-livre"] == f) {
