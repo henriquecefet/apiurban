d831d7309f801505d1e29b27a368b31d3511937e
henriquecefet <rodriguesoareshenrique@gmail.com>
2021-01-22 12:44:55 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -45,8 +45,8 @@ function lerCidades(){
                    adicionarImagem(id,jx.hotspot[i].imagem, "200px", "320px");
                    adicionarParagrafo(id, jx.hotspot[i].nome);
                    adicionarParagrafo(id, "Situação da pandemia:");
-                   adicionarParagrafo(id, "Crescimento diário de casos:"+jx.situacao_covid.crescimento_casos+"%");
-                   adicionarParagrafo(id, "Crescimento diário de óbitos:"+jx.situacao_covid.crescimento_mortes+"%");
+                   adicionarParagrafo(id, "Crescimento diário de casos: "+parseFloat(jx.situacao_covid.crescimento_casos.toFixed(2))+"%");
+                   adicionarParagrafo(id, "Crescimento diário de óbitos: "+parseFloat(jx.situacao_covid.crescimento_mortes.toFixed(2))+"%");
                    for(let j = 0; j<jx.hotspot[i].recomendacaoFutura.length; j++){
                       adicionarParagrafo(id, jx.hotspot[i].recomendacaoFutura[j][0]+" - "+jx.hotspot[i].recomendacaoFutura[j][1]);
                       adicionarParagrafo(id,jx.hotspot[i].recomendacaoFutura[j][2])
