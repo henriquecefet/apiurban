836a79ff81ca354e5dc46b684bde6cc6dd013561
Henrique Soares Rodrigues <rodriguesoareshenrique@gmail.com>
2022-08-16 16:14:16 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,5 @@
 <?php
-$db;
-$host        = "ec2-52-201-59-194.compute-1.amazonaws.com";
+$host        = "host = ec2-52-201-59-194.compute-1.amazonaws.com";
 $port        = "port = 5432";
 $dbname      = "dbname = d6pg29oggq3lk3";
 $credentials = "user = jptkmcecjrktsz password=afe78295478b7ba5cf4f94d0107b2dc9df6451c132d051725ac22540f2e5023f";
