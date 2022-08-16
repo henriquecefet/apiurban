dd9176d8bb01c0a8d0342615d46e6d5f288c1e8e
Henrique Soares Rodrigues <rodriguesoareshenrique@gmail.com>
2022-08-15 22:09:48 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 $db;
-$host        = "ec2-3-222-183-44.compute-1.amazonaws.com";
+$host        = "ec2-52-201-59-194.compute-1.amazonaws.com";
 $port        = "port = 5432";
-$dbname      = "dbname = d5652rk5upj4qb";
-$credentials = "user = pdgjnondfgzvnr password=afb1574a2e12a2dee262b16e727cc67fee67cd8cdef5f40c01fb1f0f8faf791e";
+$dbname      = "dbname = d6pg29oggq3lk3";
+$credentials = "user = jptkmcecjrktsz password=afe78295478b7ba5cf4f94d0107b2dc9df6451c132d051725ac22540f2e5023f";
 $db = pg_connect( "$host $port $dbname $credentials"  );
 	if(!$db) {
 	      //echo "Error : Unable to open database\n";
